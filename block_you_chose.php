a5d492f081aec999a3b1649b80e410625732dbd3
Simone Ferrero <simone.ferrero@outlook.com>
2016-09-30 13:12:30 +0100

    fix image url to fetch from local

--- block_you_chose.php
+++ block_you_chose.php
@@ -7,7 +7,7 @@ class block_you_chose extends block_list
 
     public function get_content()
     {
-        global $COURSE;
+        global $COURSE, $CFG;
 
         $course_id=$COURSE->id;
 
@@ -20,10 +20,7 @@ class block_you_chose extends block_list
         $this->content->icons  = array();
         $this->content->footer = get_string('footer', 'block_you_chose');
 
-        /*NOT WORKING: moodle can't find the resource*/
-        //$this->content->items[] = html_writer::img('images\templar.png', get_string('choose_img', 'block_you_chose'));
-
-        $this->content->items[] = html_writer::tag('img', '', array('src' => 'http://vignette3.wikia.nocookie.net/indianajones/images/6/67/Knight.jpg/revision/latest?cb=20080328211709'));
+        $this->content->items[] = html_writer::empty_tag('img', array('src' => '../blocks/you_chose/images/templar.jpg'));
         $this->content->icons[] = "";
 
         $this->content->items[] = html_writer::tag('div', get_string('empty', 'block_you_chose'));
@@ -37,7 +34,7 @@ class block_you_chose extends block_list
             $this->content->icons[] = '';
 
             $this->content->items[] = html_writer::tag('a', get_string('choose_' . $i, 'block_you_chose'), array('href' => "/blocks/you_chose/choice.php?chalice={$i}&course_id={$course_id}"));
-            $this->content->icons[] = html_writer::empty_tag('img', array('src' => 'https://yppedia.puzzlepirates.com/images/9/96/Chalice.png', 'class' => 'icon'));
+            $this->content->icons[] = html_writer::empty_tag('img', array('src' => '../blocks/you_chose/images/icons/Chalice.png', 'class' => 'icon'));
             /*NOT WORKING: moodle can't find the resource*/
             //$this->content->icons[] = html_writer::empty_tag('img', array('src' => 'images/icons/chalice.png', 'class' => 'icon'));
         }
--- choice.php
+++ choice.php
@@ -29,10 +29,10 @@ echo $OUTPUT->heading(get_string('you_chose', 'block_you_chose'));
 if ($chalice === 3)
 {
     echo '<h4>' . get_string('wisely', 'block_you_chose') . '</h4><br />';
-    echo '<img src="http://earnthis.net/wp-content/uploads/2013/10/55658.jpg" /><br /><br />';
+    echo '<img src="images/wisely.jpg" /><br /><br />';
 } else {
     echo '<h4>' . get_string('poorly', 'block_you_chose') . '</h4><br />';
-    echo '<img src="http://3.bp.blogspot.com/_cvFfOEje0HM/TJeQnssblLI/AAAAAAAAAPs/KCtQx6ZMlZo/s1600/Grail-wrong.jpg" /><br /><br />';
+    echo '<img src="images/poorly.jpg" /><br /><br />';
 }
 
 echo '<h3><a href="../../course/view.php?id=' . $course_id . '">Click here to return to the course</a></h3>';
